18414e0e0f908fd47415c70cf4fb76b579db784a
ismail2009 <somaaa2009@live.com>
2018-09-12 12:19:57 +0300

    add facebook and inestgram url

--- src/components/Common/Footer/index.js
+++ src/components/Common/Footer/index.js
@@ -8,8 +8,8 @@ const Footer = () => (
       <div className="footer_socail">
         <p>تابعونا صفحتنا على:</p>
         <div className="footer_socail_links">
-          <FooterWrapper.SocialLink to="#" onClick="ga('send', 'event', 'Facebook Page');"><div><TiSocialFacebook /></div></FooterWrapper.SocialLink>
-          <FooterWrapper.SocialLink to="#" onClick="ga('send', 'event', 'Instagram Page');"><div><TiSocialInstagram /></div></FooterWrapper.SocialLink>
+          <FooterWrapper.SocialLink to="https://business.facebook.com/GDGGlobalDentalGateway" onClick="ga('send', 'event', 'Facebook Page');"><div><TiSocialFacebook /></div></FooterWrapper.SocialLink>
+          <FooterWrapper.SocialLink to="https://www.instagram.com/globaldentalgatewaygdg/" onClick="ga('send', 'event', 'Instagram Page');"><div><TiSocialInstagram /></div></FooterWrapper.SocialLink>
         </div>
       </div>
     </FooterWrapper.FooterContainer>
